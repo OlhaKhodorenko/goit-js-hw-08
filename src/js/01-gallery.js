0e7690a4607c32aaff1c48b3bbb3b6b8de868848
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-05-02 15:03:59 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMmarkup = createGallaryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMmarkup);
+
+
+function  createGallaryMarkup (galleryItems){
+return galleryItems.map(({
+    preview, original, description}) => {
+    return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}).join("");
+}
+      const lightbox = new SimpleLightbox('.gallery a',
+       {docClose: false,
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+    
