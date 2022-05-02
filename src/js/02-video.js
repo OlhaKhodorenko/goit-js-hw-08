58d408bfa81010579c4377201c0ce33cf8204ebd
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-05-02 15:04:03 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const storageKey = "videoplayer-current-time";
+
+    player.on('timeupdate', throttle((data) =>
+        localStorage.setItem(storageKey, JSON.stringify(data.seconds),1000)
+    ));
+       
+      
+    const seconds = localStorage.getItem("videoplayer-current-time");
+        if(seconds){
+            player.setCurrentTime(seconds);
+        };
+    
